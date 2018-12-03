5ded534e01ab6de47c54823f0f278ec5c65db71a
Matthew Shelley <dewayne.shelley@gmail.com>
2018-12-03 01:21:50 -0800

    updating test

--- src/setup/__test__/helper.test.ts
+++ src/setup/__test__/helper.test.ts
@@ -1,10 +1,28 @@
+const findUp = jest.fn();
+const readConfig = jest.fn();
+const writeConfig = jest.fn();
+
+// We just need to know these are triggered
+jest.mock('find-up', () => findUp);
+jest.mock('../config', () => ({ readConfig, writeConfig }));
+
 // Internal
 import { checkForSettings } from '../helper';
 
 describe('helper', () => {
   describe('checkForSettings', () => {
+    let fn;
+
+    beforeEach(() => {
+      fn = checkForSettings();
+    });
+
     it('should be a function', () => {
       expect(typeof checkForSettings).toBe('function');
+    });
+
+    it('should call these methods', () => {
+      expect(findUp).toHaveBeenCalledWith('.generator.config');
     });
   });
 });
